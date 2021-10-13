d8cfe41ed5daa9af6ca1c11aaea524f79eb01932
Vladimir Liankevich <vladimirlyn@gmail.com>
2021-10-13 11:16:07 +0300

    step_4 fix commponent EmployeesListItem, add onIncrease, onLikeHeandler

--- src/component/employeesListItem/employeesListItem.tsx
+++ src/component/employeesListItem/employeesListItem.tsx
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import './employeeListItem.scss';
 
 type EmployeesListItemType = {
@@ -6,31 +7,68 @@ type EmployeesListItemType = {
   increase?: boolean;
 };
 
-export const EmployeesListItem = (props: EmployeesListItemType) => {
-  const { name, salary, increase } = props;
-  return (
-    <li
-      className={
-        'list-group-item d-flex justify-content-between ' +
-        `${increase && 'increase'}`
-      }
-    >
-      <span className="list-group-item-list">{name}</span>
-      <input
-        type="text"
-        className="list-group-item-input"
-        defaultValue={salary + '$'}
-      />
-      <div className="d-flex justify-content-center align-items-center">
-        <button type="button" className="btn-cookie btn-sm">
-          <i className="fas fa-cookie"></i>
-        </button>
-
-        <button type="button" className="btn-trash btn-sm">
-          <i className="fas fa-trash"></i>
-        </button>
-        <i className="fas fa-star"></i>
-      </div>
-    </li>
-  );
+type StateType = {
+  increase: boolean;
+  like: boolean;
 };
+
+export class EmployeesListItem extends React.Component<
+  EmployeesListItemType,
+  StateType
+> {
+  constructor(props: EmployeesListItemType) {
+    super(props);
+    this.state = {
+      increase: false,
+      like: false,
+    };
+  }
+
+  onIncrease = () => {
+    this.setState(({ increase }) => ({
+      increase: !increase,
+    }));
+  };
+  onLikeHandler = () => {
+    this.setState(({ like }) => ({
+      like: !like,
+    }));
+  };
+
+  render() {
+    const { name, salary } = this.props;
+    const { increase, like } = this.state;
+    return (
+      <li
+        className={
+          'list-group-item d-flex justify-content-between like' +
+          `${increase && 'increase'}` +
+          `${like && ' like'}`
+        }
+      >
+        <span className="list-group-item-list" onClick={this.onLikeHandler}>
+          {name}
+        </span>
+        <input
+          type="text"
+          className="list-group-item-input"
+          defaultValue={salary + '$'}
+        />
+        <div className="d-flex justify-content-center align-items-center">
+          <button
+            type="button"
+            className="btn-cookie btn-sm"
+            onClick={this.onIncrease}
+          >
+            <i className="fas fa-cookie"></i>
+          </button>
+
+          <button type="button" className="btn-trash btn-sm">
+            <i className="fas fa-trash"></i>
+          </button>
+          <i className="fas fa-star"></i>
+        </div>
+      </li>
+    );
+  }
+}
